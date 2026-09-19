3670eb036b582a37ce8ba4297da3a20420845252
Kevin Mirco Hardt <hardt.kevin.mirco@gmail.com>
2026-09-19 17:28:50 +0200

    setup shadcn

--- apps/desktop/electron.vite.config.ts
+++ apps/desktop/electron.vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     resolve: {
       alias: {
         "@renderer": resolve("src/renderer/src"),
+        "@": resolve("src/renderer/src"),
       },
     },
     plugins: [react(), tailwindcss()],
